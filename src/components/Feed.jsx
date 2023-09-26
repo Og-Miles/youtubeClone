87560db8b9878f8dcdcfa27a7d3debff9766889d
Og-Miles <emmanuelogbuji5@gmail.com>
2023-09-26 10:50:25 +0100

    feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,49 +1,61 @@
-import { useState, useEffect} from 'react';
-import { Box, Stack, Typography } from '@mui/material';
-import {Sidebar, Videos} from './';
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { Sidebar, Videos } from "./";
 
-import {fetchFromAPI } from '../utils/fetchFromAPI'
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
 
-  const [selectedCategory, setSelectedCategory] = useState('New')
-
-  useEffect(() =>{ 
-    fetchFromAPI(`search?part=snippetq=${selectedCategory}`)
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippetq=${selectedCategory}`);
   }, [selectedCategory]);
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-    <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <Sidebar 
-        selectedCategory = {selectedCategory}
-        setSelectedCategory = {setSelectedCategory}
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
         />
-       
 
-      
-        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+        <Typography
+          className='copyright'
+          variant='body2'
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
           Copyright © 2023 Miles
         </Typography>
+      </Box>
+      <Box
+        p={2}
+        sx={{
+          overflowY: "auto",
+          height: "90vh",
+          flex: 2,
+        }}
+      >
+        <Typography
+          variant='h4'
+          fontWeight='bold'
+          mb={2}
+          sx={{
+            color: "white",
+          }}
+        >
+          {" "}
+          {selectedCategory}
+          <span style={{ color: "#F31503" }}> videos</span>
+        </Typography>
 
-      
-    </Box>
-        <Box p={2} sx={{ 
-          overflowY: 'auto',
-          height: '90vh',
-          flex: 2
-
-        }}>
-          <Typography variant='h4'
-          fontWeight= 'bold' mb={2} sx= {{
-            color: 'white'
-          }} > {selectedCategory} 
-            <span style ={{color: '#F31503'}}> videos
-            </span>
-          </Typography>
-
-          <Videos/>
-        </Box>
+        <Videos videos={[]} />
+      </Box>
     </Stack>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
