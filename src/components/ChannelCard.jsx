0bcd3af8775462de248a5386428d1f28d19e32d4
Og-Miles <emmanuelogbuji5@gmail.com>
2023-10-05 13:16:49 +0100

    tweaks

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,4 +1,4 @@
-import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { Box, CardMedia, Typography } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
