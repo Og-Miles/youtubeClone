bc93a6f20ddea4d0abea7349132f51fb242ccf69
Og-Miles <emmanuelogbuji5@gmail.com>
2023-10-05 12:43:42 +0100

    search fuctionality

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -11,8 +11,8 @@ const Videos = ({ videos }) => {
     <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
       {videos.map((item, idx) => (
         <Box key={idx}>
-          {item.id.videoId && <VideoCard video={item} />}
-          {item.id.channelId && <ChannelCard channelDetail={item} />}
+          {item.id.videoId && <VideoCard video={ item } />}
+          {item.id.channelId && <ChannelCard channelDetail={ item } />}
         </Box>
       ))}
     </Stack>
