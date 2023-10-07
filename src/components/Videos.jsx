aef8be6a30b1d7db612ae874e49066f7e8694f3d
Og-Miles <emmanuelogbuji5@gmail.com>
2023-10-07 21:35:00 +0100

    video rendering

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,18 +1,23 @@
 import { Stack, Box } from "@mui/material";
 import { ChannelCard, VideoCard } from ".";
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
   // Check if videos is an array and not undefined
   if (!Array.isArray(videos)) {
     return null; // Or you can return an error message or handle it differently
   }
 
   return (
-    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
+    <Stack
+      direction={direction || "row"}
+      flexWrap='wrap'
+      justifyContent='start'
+      gap={2}
+    >
       {videos.map((item, idx) => (
         <Box key={idx}>
-          {item.id.videoId && <VideoCard video={ item } />}
-          {item.id.channelId && <ChannelCard channelDetail={ item } />}
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
       ))}
     </Stack>
